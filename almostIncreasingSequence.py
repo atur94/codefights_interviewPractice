0a392cc5c0ba2595aeadfa0e3b45ecd41ab77af0
atur94 <atur94@gmail.com>
2017-08-26 13:44:17 +0200

    WORKING ALMOSTINCREASINGSEQUENCE

---
+++ almostIncreasingSequence.py
@@ -0,0 +1,10 @@
+def almostIncreasingSequence(seq):
+    print(seq[0])
+    count = 0
+    for i in range(len(sequence)):
+
+            count = count + 1
+    return count <= 1
+
+sequence = [1, 3, 2, 1]
+print(almostIncreasingSequence(sequence))
