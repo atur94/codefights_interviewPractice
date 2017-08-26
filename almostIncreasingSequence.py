ad61a8f2e04a9572b013878acc6993db206a5846
atur94 <atur94@gmail.com>
2017-08-26 14:05:01 +0200

    33/34

--- almostIncreasingSequence.py
+++ almostIncreasingSequence.py
@@ -1,10 +1,21 @@
 def almostIncreasingSequence(seq):
-    print(seq[0])
+    tests_left = True
     count = 0
-    for i in range(len(sequence)):
+    if len(set(seq)) <= len(seq) - 2:
+        return False
+    for i in range(1, len(seq)-1):
 
+        if seq[i] >= seq[i+1] or (i == 1 and seq[i-1] >= seq[i]):
             count = count + 1
+            if tests_left:
+                if seq[i + 1] <= seq[i - 1]:
+                    if i < (len(seq) - 2):
+                        if seq[i+2] <= seq[i]:
+                            return False
+                tests_left = False
+            else:
+                return False
     return count <= 1
 
-sequence = [1, 3, 2, 1]
+sequence = [10, 1, 2, 3, 4, 5, 6, 1]
 print(almostIncreasingSequence(sequence))
