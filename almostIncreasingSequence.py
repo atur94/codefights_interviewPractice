db1c464008edc582782fffe21521c4371e3d95f8
atur94 <atur94@gmail.com>
2017-08-26 15:28:40 +0200

    34/34 but u need to delete prints. PYTHON 3.5

--- almostIncreasingSequence.py
+++ almostIncreasingSequence.py
@@ -1,21 +1,24 @@
 def almostIncreasingSequence(seq):
     tests_left = True
     count = 0
-    if len(set(seq)) <= len(seq) - 2:
-        return False
-    for i in range(1, len(seq)-1):
-
-        if seq[i] >= seq[i+1] or (i == 1 and seq[i-1] >= seq[i]):
+    for i in range(0, len(seq)-1):
+        print(i)
+        if seq[i] >= seq[i+1]:
             count = count + 1
+            print(seq[i], seq[i+1], i)
             if tests_left:
-                if seq[i + 1] <= seq[i - 1]:
+                if seq[i + 1] <= seq[i - 1] and i>0:
+                    print("1 in: ", seq[i+1], seq[i-1])
                     if i < (len(seq) - 2):
                         if seq[i+2] <= seq[i]:
+                            print("2 in: ", seq[i + 2], seq[i])
                             return False
+
                 tests_left = False
             else:
                 return False
     return count <= 1
 
-sequence = [10, 1, 2, 3, 4, 5, 6, 1]
+sequence = [1, 2, 2, 3]
 print(almostIncreasingSequence(sequence))
+
