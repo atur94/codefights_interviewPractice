e4543763aafbe99f2c41832ae1db7a25fa70c3a1
atur94 <atur94@gmail.com>
2017-09-03 13:17:34 +0200

    MAtrix element sum PASSED

--- matrixElementsSum.py
+++ matrixElementsSum.py
@@ -1,4 +1,11 @@
 def matrixElementsSum(matrix):
+    sum = 0
+    for i in range(len(matrix[0])):
+        for j in range(len(matrix)):
+            if matrix[j][i] == 0:
+                break
+            sum = sum + matrix[j][i]
+    return sum
 
 mat   = [[0, 1, 1, 2],
          [0, 5, 0, 0],
