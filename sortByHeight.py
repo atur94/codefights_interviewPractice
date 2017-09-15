6c5ffb9e4500fbd22c24f67c147e4496f95ab0c8
atur94 <atur94@gmail.com>
2017-09-15 17:06:54 +0200

    sortByHeight

--- sortByHeight.py
+++ sortByHeight.py
@@ -1,19 +1,16 @@
 def sortByHeight(a):
-    indexes = []
-    sortedOut = []
-    returned = []
-    for i in range(0, len(a)):
-        if a[i] == -1:
-            indexes.append(i)
-        else:
-            sortedOut.append(a[i])
-    sortedOut = sorted(sortedOut)
-    for i in range(0, len(sortedOut)):
-        j = 0
-        if indexes[j] == i:
-            returned.append(-1)
-            j += 1
-        returned.append(sortedOut[i])
-    return sorted(a)
-toSort = [-1, 150, 190, 170, -1, -1, 160, 180]
-print(sortByHeight(toSort))
+    trees = []
+    rel = []
+    for tree in a:
+        if tree != -1:
+            trees.append(tree)
+    trees = sorted(trees)
+    for tree in a:
+        if tree != -1:
+            rel.append(trees.pop(0))
+            continue
+        rel.append(-1)
+    return rel
+
+trees = [-1, 150, 190, 170, -1, -1, 160, 180]
+print(sortByHeight(trees))
